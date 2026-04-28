3061a870c2b5625ee3e546de3672e1c2afce2e12
jaredfriend1-spec <jaredfriend1@gmail.com>
2026-04-27 23:18:00 -0400

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,21 @@
-export const golfers = [];
-export const courses = [];
+export const golfers = [
+  { id: '1', name: 'JARED', handicap: 0 },
+  { id: '2', name: 'FRED', handicap: 0 },
+  { id: '3', name: 'MIKE', handicap: 0 },
+  { id: '4', name: 'TODD', handicap: 0 },
+];
+
+export const courses = [
+  { id: 'mcc', name: 'Montgomery Country Club', par: 72 }
+];
+
 export const tournamentSettings = {
   name: "Blitz Board Tournament",
   date: new Date().toLocaleDateString(),
 };
+
+// This is the missing piece blocking your build:
+export const BLITZ_TEAMS = [
+  { id: 't1', name: 'TEAM 1', playerIds: ['1', '2'] },
+  { id: 't2', name: 'TEAM 2', playerIds: ['3', '4'] },
+];
