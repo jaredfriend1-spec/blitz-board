9c51ddecd95a14d6b98dd93519989f8c73adebb4
jaredfriend1-spec <jaredfriend1@gmail.com>
2026-04-28 08:41:47 -0400

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -5,22 +5,11 @@ export const golfers = [
   { id: '4', name: 'TODD', handicap: 0 },
 ];
 
-export const courses = [
-  { id: 'mcc', name: 'Montgomery Country Club', par: 72 }
-];
-
-export const tournamentSettings = {
-  name: "Blitz Board Tournament",
-  date: new Date().toLocaleDateString(),
-};
-
-// Satisfies the Teams page
 export const BLITZ_TEAMS = [
   { id: 't1', name: 'TEAM 1', playerIds: ['1', '2'] },
   { id: 't2', name: 'TEAM 2', playerIds: ['3', '4'] },
 ];
 
-// Safety exports in case other pages are crashing the build
 export const PLAYERS = golfers;
-export const MATCHUPS = [];
-export const TEAM_BETS = [];
+export const courses = [{ id: 'mcc', name: 'Montgomery Country Club', par: 72 }];
+export const tournamentSettings = { name: "Blitz Board", date: new Date().toLocaleDateString() };
