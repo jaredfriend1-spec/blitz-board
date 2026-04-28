484103e387f9947ee44e0ef1688ffb23a995116b
jaredfriend1-spec <jaredfriend1@gmail.com>
2026-04-27 22:08:42 -0400

    Create data.ts

---
+++ lib/data.ts
@@ -0,0 +1,11 @@
+// lib/data.ts
+export const BLITZ_TEAMS = [
+  { id: "t1", name: "Team 1", players: ["Andrew Sovero", "Sam Oshrine", "Ira Cohen", "Josh Bumgarner"] },
+  { id: "t2", name: "Team 2", players: ["Rick Sovero", "Jeff Perkins", "Tony Zamostny", "George Blyth IV"] }
+];
+
+export const COURSE = [
+  { hole: 1, par: 4 }, { hole: 2, par: 4 }, { hole: 3, par: 3 },
+  { hole: 4, par: 3 }, { hole: 5, par: 4 }, { hole: 6, par: 4 },
+  { hole: 7, par: 5 }, { hole: 8, par: 4 }, { hole: 9, par: 4 }
+];
